e8ca06ea2688860adc4ee82789f9dd7727585d78
Gonzalo <gvm2121@gmail.com>
2021-05-31 22:44:36 -0400

    primer modelo de sistema rfi

--- fynsa_src/fynsa/RFI/models.py
+++ fynsa_src/fynsa/RFI/models.py
@@ -69,7 +69,102 @@ class rfi_generacion_comite_temporal(models.Model):
 
 
 
+class rfi_bonos(models.Model):
+    #esta class es para los datos del sistema de posiciones
+    cusip = models.TextField(null=True,blank=True)
+    security_name = models.TextField(null=True,blank=True)
+    short_name = models.TextField(null=True,blank=True)
+    bb_composite = models.CharField(max_length=4,default='NR',null=True,blank=True)
+    payment_rank = models.CharField(max_length=40,null=True,blank=True)
+    cntry_of_risk = models.CharField(max_length=2,null=True,blank=True)
+    yas_mod_dur = models.DecimalField(max_digits=7, decimal_places=3,default=0,null=True,blank=True)
+    yas_bond_yld = models.DecimalField(max_digits=7, decimal_places=3,default=0,null=True,blank=True)
+    classification = models.TextField(default='-',null=True,blank=True)
+    industria = models.TextField(null=True,blank=True)
+    ising = models.TextField(null=True,blank=True)
+    risk = models.TextField(default='NR',null=True,blank=True)
+    dur_text = models.TextField(null=True,blank=True)
+    tasa = models.IntegerField(default='',null=True,blank=True)
+    fecha_subida = models.DateField(null=True,blank=True)
+    maturity = models.DateField(null=True,blank=True)
 
+    def __str__(self):
+        return self.short_name
+        
+    def save(self,*args, **kwargs):
+        categorias = {
+        'Food and Beverage':'Retail',
+        'Midstream':'O&G',
+        'Independent':'O&G',
+        'Natural Gas':'O&G',
+        'Papper' : 'Pulpa y Papel',
+        'Airlines' : 'Aerolineas',
+        'Aerospace/Defense' : 'Aerolineas',
+        'Banking':'Banco y Financieras',
+        'Brokerage Assetmanagers Exchanges':'Banco y Financieras',
+        'Other Financial':'Banco y Financieras',
+        'Finance Companies':'Banco y Financieras',
+        'Government Owned, No Guarantee':'Quasisov',
+        'Goverment Sponsored':'QuasiSov',
+        'Electric' : 'Utilities',
+        'Local Authority' : 'Utilities',
+        'Wireless' : 'Telecom',
+        'Wirelines' : 'Telecom',
+        'Media Entertainment' : 'Telecom',
+        'Cable Satellite' : 'Telecom',
+        'Railroads' : 'Transporte',
+        'Transportation Services' : 'Transporte',
+        'Metal and Mining':'Mineria y Minerales',
+        'Chemicals':'Químicos',
+        'Building Materials':'Cemento y Construcción',
+        'Other Industrial':'Real State',
+        'Retailers':'Retail',
+        'Gaming':'Retail',
+        'Consumer Products':'Retail',
+    }
+        risk_dic ={
+            'AAA':'IG',
+            'AAA-':'IG',
+            'AA':'IG',
+            'AA+':'IG',
+            'AA-':'IG',
+            'A':'IG',
+            'A+':'IG',
+            'A-':'IG',
+            'BBB':'IG',
+            'BBB+':'IG',
+            'BBB-':'HY',
+            'BB':'HY',
+            'BB+':'HY',
+            'BB-':'HY',
+            'B':'HY',
+            'B+':'HY',
+            'B-':'HY',
+            'CCC':'HY',
+            'CCC+':'HY',
+            'CCC-':'HY',
+            'CC':'HY',
+            'CC+':'HY',
+            'CC-':'HY',
+            'C':'HY',
+            'C+':'HY',
+            'C-':'HY',
+            'DDD':'HY',
+            'DDD+':'HY',
+            'DDD-':'HY',
+            'DD':'HY',
+            'DD+':'HY',
+            'DD-':'HY',
+            'D':'HY',
+            'D+':'HY',
+            'D-':'HY',
+            
+        }
+        cat = categorias[self.classification] or None
+        self.industria = cat
+        r = risk_dic[self.bb_composite] or 'NR'
+        self.dur_text = r
+        super().save(*args, **kwargs)
 
 
 
